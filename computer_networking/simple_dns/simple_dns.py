d2e85cfce5d20d172538a8e8ae14dad37729f228
tre3p <whereareyo@yahoo.com>
2024-09-02 16:31:08 +0300

    WIP on rewriting dns client

--- computer_networking/simple_dns/simple_dns.py
+++ computer_networking/simple_dns/simple_dns.py
@@ -1,60 +1,75 @@
 import socket
 import struct
 import sys
+import random
 
-def send_dns_req(socket, domain_name, dns_address):
-    xid = 42
-    flags = 0x0100
-    query = struct.pack('!HHHHHH', xid, flags, 1, 0, 0, 0)
+DNS_ADDRESS = "8.8.8.8"
+DNS_DEFAULT_PORT = 53
 
+def send_dns_request(socket, dns_address, dns_port, query):
+    print("Sending DNS request to " + dns_address)
+    socket.sendto(query, (dns_address, dns_port))
+    
+def receive_dns_response(socket):
+    resp_bytes, sender = socket.recvfrom(4093)
+    print("Received response from " + str(sender[0]))
+    return resp_bytes
+    
+def construct_dns_request(domain_name):
+    id = random.randint(0, 65535)
+    qdcount = 1
+    
+    flags = 0x00
+    flags |= 0x0100 # recursion query desired
+    
+    message = struct.pack('!HHHHHH', id, flags, qdcount, 0, 0, 0)
+    
     for part in domain_name.split('.'):
-        query += len(part).to_bytes(1, "big")
-        query += part.encode('ascii')
-
-    query += struct.pack('!cHH', b'\x00', 1, 1)
-    socket.sendto(query, (dns_address, 53))
-
-def process_response(response_bytes):
-    id, flags, qdcount, ancount, nscount, arcount = struct.unpack('!HHHHHH', response_bytes[:12])
-    print("Response ID: " + str(id))
-    response_code = flags & 0x0F
-    if response_code != 0:
-        print("Response code is " + str(response_code))
+        message += len(part).to_bytes(1, 'big')
+        message += part.encode('ascii')
+    
+    message += b'\x00' # terminate hostname
+    
+    qtype = 1
+    qclass = 1
+    
+    message += struct.pack("!HH", qtype, qclass)
+    
+    return (id, message)
+    
+def process_dns_response(response_bytes):
         return
+        
+def validate_dns_response(response_bytes, request_id):
+    response_id, flags = struct.unpack("!HH", response_bytes[:4])
+    if response_id != request_id:
+        print("Response received with ID " + str(id) + ". However, request ID was " + str(request_id))
+        return False
+        
+    if ((flags & 0x8000) >> 15) != 1:
+        print("Received response isn't response for original request")
+        return False
+        
+    rcode = flags &0x000F
+    if rcode != 0:
+        print("Received RCODE isn't 0. RCODE: " + str(rcode))
+        return False
+        
+    return True
 
-    question_section = response_bytes[12:]
-    question_section_ptr = 0
-
-    while question_section[question_section_ptr] != 0x00:
-        part_len = int.from_bytes(question_section[question_section_ptr:question_section_ptr+1], 'big')
-        question_section_ptr += (part_len + 1)
-
-    question_section_ptr += 2 + 2 + 1 # 1 for step from '0' and 2 + 2 for qtype and qclass
-
-    response_section = question_section[question_section_ptr:]
-    print(response_section)
-    response_section_ptr = 0
-    response_name_parts = []
-
-    while response_section[response_section_ptr] != 0:
-        part_len = int.from_bytes(response_section[response_section_ptr:response_section_ptr+1], 'big')
-        print(part_len)
-        actual_part = response_section[response_section_ptr+1:response_section_ptr+1+part_len].decode('ascii')
-        response_name_parts.append(actual_part)
-        print(actual_part)
-        response_section_ptr += part_len + 1
-
-    response_section_ptr += 1 # to step from '0'
-
-
-
+def dns_lookup(dns_address, domain_name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    (req_id, message) = construct_dns_request(domain_name)
+    send_dns_request(sock, DNS_ADDRESS, DNS_DEFAULT_PORT, message)
+    resp_bytes = receive_dns_response(sock)
+    if validate_dns_response(resp_bytes, req_id):
+        process_dns_response(resp_bytes)
+    else:
+        print("Received response isn't valid, aborting DNS lookup..")
 
 if __name__ == "__main__":
-    domain = sys.argv[1]
-    dns_address = '8.8.8.8'
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    send_dns_req(s, domain, dns_address)
-
-    resp_bytes, sender = s.recvfrom(4093)
-    process_response(resp_bytes)
+    if len(sys.argv) <= 1:
+        print("Usage: simple_dns *domain*")
+    else:
+        domain = sys.argv[1]
+        dns_lookup(DNS_ADDRESS, domain)
