e4199881c7462dbfec0eca254abd99696bdfc5cd
tre3p <whereareyo@yahoo.com>
2024-12-23 12:03:57 +0400

    improve num_to_roman iterative approach

--- algo/problem_solving/roman_numeral.py
+++ algo/problem_solving/roman_numeral.py
@@ -1,28 +1,30 @@
 mappings = {
-    1: "I",
-    4: "IV",
-    5: "V",
-    9: "IX",
-    10: "X",
-    40: "XL",
-    50: "L",
-    90: "XC",
-    100: "C",
-    400: "CD",
-    500: "D",
+    1000: "M",
     900: "CM",
-    1000: "M"
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I",
 }
 
 def convert_to_roman_iter(num):
-    result = ""
+    result = []
 
     while num != 0:
-        m = max([n for n in mappings if num - n >= 0])
-        num -= m
-        result += mappings[m]
+        for n, m in mappings.items():
+            if num - n >= 0:
+                result.append(m)
+                num -= n
+                break
 
-    return result
+    return ''.join(result)
 
 
 def convert_to_roman_recursive(num):
